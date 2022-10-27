fecae89fdcb8a8cd98a23677ce326b4d9e9190e3
ANNE LUND <ANNELS87@HOTMAIL.com>
2022-10-27 09:56:31 +0200

    ...

--- pages/api/anne.js
+++ pages/api/anne.js
@@ -11,11 +11,11 @@ async function handler(req, res) {
 
     if(req.method === "PUT") {
         const {titel, image, id, url, description, købt} = req.body
-        if(!id || !købt){
-            res.status(422).json({message: "Invalid data"})
-            return;
-        }
-await executeQuery(`UPDATE anne SET(titel, description, image, url, købt) VALUES('${titel}', '${description}', '${image}', '${url}', '${købt}') WHERE id='${id}'`)
+        // if(!id || !købt){
+        //     res.status(422).json({message: "Invalid data"})
+        //     return;
+        // }
+await executeQuery(`UPDATE anne SET titel = '${titel}', image = '${image}', url = '${url}', description = '${description}', købt = '${købt}' WHERE id = '${id}'`)
 res.status(201).json({message: "Data updated!"})
 return;
     }
