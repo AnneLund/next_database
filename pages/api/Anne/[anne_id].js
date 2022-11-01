d152070d7a5d9de0c3d8fed60aa46600400b3570
ANNE LUND <ANNELS87@HOTMAIL.com>
2022-11-01 16:47:20 +0100

    ...

--- pages/api/Anne/[anne_id].js
+++ pages/api/Anne/[anne_id].js
@@ -16,7 +16,7 @@ await NextCors(req, res, {
           query: { anne_id },
         } = req;
 
-        await executeQuery(`SELECT * FROM anne WHERE id = '${id}'`)
+        const data = await executeQuery(`SELECT * FROM anne WHERE id = '${id}'`)
         res.status(201).json({message: "Data fetch", data})
         res.send({msg: `Hello ${anne_id}`});  
         return;
