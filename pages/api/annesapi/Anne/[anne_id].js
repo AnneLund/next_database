b326abae1a7b790e0b7aa27f63984f544c0551c6
ANNE LUND <ANNELS87@HOTMAIL.com>
2022-11-01 18:56:43 +0100

    ...

--- pages/api/annesapi/Anne/[anne_id].js
+++ pages/api/annesapi/Anne/[anne_id].js
@@ -1,4 +1,25 @@
+import executeQuery from "../../../Source/db";
+import NextCors from 'nextjs-cors'
+
 module.exports = (req, res) => {
+  async function handler(req, res){
+   await NextCors(req, res, {
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccesStatus: 200,
+})   
+
+if(req.method === "GET") {
+  const data = await executeQuery(`SELECT * FROM anne`)
+  res.status(201).json({message: "Data fetch", data})
+  return;
+  }
+  else{
+      res.status(500).json({message: "Route not valid"})
+  }
+
+  }
+
   const {
       query: { anne_id },
     } = req;
